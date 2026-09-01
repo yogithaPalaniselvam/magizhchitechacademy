38789f7f676cee7a5359584bde778b8530eda335
yogithaPalaniselvam <yogitha437@gmail.com>
2026-09-01 11:53:57 +0530

    uploade

---
+++ name.java
@@ -0,0 +1,8 @@
+class Yogi
+{
+public static void main(String [] args)
+{
+	System.out.println("Ten name: \n");
+	System.out.println("ADHAV\nKRISHNA\nJEEVAN\nJENI\nSIDHI\nYOGITHA\nZEWE\nZIBI");
+}
+}
