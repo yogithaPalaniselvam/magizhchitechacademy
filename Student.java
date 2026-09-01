ac13a2e31b0bde8c4b2ee0fbccc9dbccd11a1f3b
yogithaPalaniselvam <yogitha437@gmail.com>
2026-09-01 17:09:59 +0530

    student program uploaded

---
+++ Student.java
@@ -0,0 +1,8 @@
+class Student
+{
+public static void main(String [] ar)
+{
+System.out.println("Student details:");
+System.out.println("deatails include NAME AGE CLASS GRADE");
+}
+}
--- name.java
+++
@@ -1,8 +0,0 @@
-class Yogi
-{
-public static void main(String [] args)
-{
-	System.out.println("Ten name: \n");
-	System.out.println("ADHAV\nKRISHNA\nJEEVAN\nJENI\nSIDHI\nYOGITHA\nZEWE\nZIBI");
-}
-}
