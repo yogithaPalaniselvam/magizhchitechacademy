5c4da55ed75088128760072a1814c451c852ac8c
yogithaPalaniselvam <yogitha437@gmail.com>
2026-09-07 16:59:24 +0530

    sep7 HOMEWORK uploaded

---
+++ YogDetail.java
@@ -0,0 +1,28 @@
+class YogDetail
+{
+public static void main(String [] ar)
+{
+String name="YOGITHA";
+char ini='P';
+byte age=27;
+Short rent=5000;
+int expense=250000;
+long adhaar=1232212332255l;
+boolean voted=true;
+float salary=500000f;
+double mutualInterest =356099987.76545;
+System.out.println("--------------------------------------------");
+System.out.println("         YOGITHA DETAILS");
+System.out.println("--------------------------------------------");
+System.out.println("NAME           : "+name);
+System.out.println("INITIAL        : "+ini);
+System.out.println("AGE            : "+age);
+System.out.println("RENT           : "+rent);
+System.out.println("YEARLY EXPENSE : "+expense);
+System.out.println("ADHAAR NUMBER  : "+ adhaar);
+System.out.println("VOTED   ?        "+ voted);
+System.out.println("SAlARY         : "+salary);
+System.out.println("MUTUALFUND     : "+mutualInterest);
+System.out.println("---------------------------------------------");
+}
+}
