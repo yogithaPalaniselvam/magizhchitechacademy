63eaa37bdd322e61f6867b17dd311e963c506c79
yogithaPalaniselvam <yogitha437@gmail.com>
2026-09-08 15:34:07 +0530

    SEP 8 WORK UPLOADED

---
+++ Calc.java
@@ -0,0 +1,20 @@
+
+class Calc 
+{
+public static void main(String ar[])
+{
+int a,b;
+float d;
+a=40;b=25;
+d=a/b;
+System.out.println("------------------------------");
+System.out.println("+++++SIMPLE CALCULATOR+++++");
+System.out.println("------------------------------");
+
+System.out.println("ADDITION OF       "+a+" + "+b+" ="+(a+b));
+System.out.println("SUBTRACTION OF    "+a+" - "+b+" ="+(a-b));
+System.out.println("MULTIPLICATION OF "+a+" * "+b+" ="+(a*b));
+System.out.println("DIVISION OF       "+a+" / "+b+" ="+d);
+System.out.println("------------------------------");
+}
+}
