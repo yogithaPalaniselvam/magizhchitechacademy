a0eb3996e6bb6e24d8cc8953039959e9db365614
yogithaPalaniselvam <yogitha437@gmail.com>
2026-09-08 15:31:11 +0530

    SEP 8 WORK UPLOADED

---
+++ Relate.java
@@ -0,0 +1,22 @@
+class Relate
+{
+public static void main(String ar[])
+{
+int a=10,b=20;
+System.out.println("-----------------------------------");
+System.out.println("++++++RELATIONAL OPERATOR+++++");
+System.out.println("-----------------------------------");
+System.out.println("INPUT  :"+a+" and "+b);
+System.out.println("OUTPUT :  \n");
+System.out.println(a>b);
+System.out.println(a<b);
+System.out.println(b==a);
+System.out.println(a<=b);
+System.out.println(a>=b);
+System.out.println(a!=b);
+System.out.println("-----------------------------------");
+}
+}
+
+
+
